e8ad0dbe7926c907cc8e2a1e1960f0b8d0d29993
Alan Fernandes <allanfernds@gmail.com>
2024-06-01 12:46:16 -0300

    feat: os infos

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const osu = require('node-os-utils');
 const cpu = osu.cpu;
 const drive = osu.drive;
 const mem = osu.mem;
+const os = osu.os;
 
 const cpuInfos = async () => {
   const percentage = await cpu.usage();
@@ -33,10 +34,34 @@ const memoryInfos = async () => {
   return memInfos;
 };
 
+const osInfos = async () => {
+  const oos = await os.oos();
+  const platform = os.platform();
+  const arch = os.arch();
+  const type = os.type();
+  const uptime = os.uptime();
+  const hostname = os.hostname();
+  const ip = os.ip();
+
+  const allInfos = {
+    platform,
+    oos,
+    arch,
+    type,
+    uptime,
+    ip,
+    hostname,
+  };
+
+  console.log(allInfos);
+  return allInfos;
+};
+
 const showPcInfos = async () => {
   cpuInfos();
   driveDiskInfos();
   memoryInfos();
+  osInfos();
 };
 
 showPcInfos();
